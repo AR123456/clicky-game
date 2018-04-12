342a505bddbce63b8defad0cba145603a4387505
AR123456 <coloradoz99@gmail.com>
2018-04-12 06:19:16 -0600

    image updates and working on click

--- src/App.js
+++ src/App.js
@@ -5,32 +5,92 @@ import Title from "./components/Title";
 import friends from "./characters.json";
 import "./App.css";
 
+let topScore = 0;
+let guessesCorrect = 0;
+let message = "";
+
 class App extends Component {
   state = {
-    friends
+    friends,
+    topScore,
+    guessesCorrect,
+    message
   };
-  removeFriend = id => {
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    this.setState({ friends });
+  setClicked = id => {
+    const friends = this.state.friends;
+    const cardClicked = friends.filter(friend => friend.id === id);
+
+    if (cardClicked[0].clicked) {
+      guessesCorrect = 0;
+      message = "Bummer. Start over";
+
+      for (let i = 0; i < friends.length; i++) {
+        friends[i].clicked = false;
+      }
+      this.setState({ message });
+      this.setState({ guessesCorrect });
+      this.setState({ friends });
+    } else {
+      cardClicked[0].clicked = true;
+      guessesCorrect = guessesCorrect + 1;
+      message = "Well Done!";
+
+      if (guessesCorrect > topScore) {
+        topScore = guessesCorrect;
+        this.setState({ topScore });
+      }
+      friends.sort((a, b) => {
+        return 0.5 - Math.random();
+      });
+      this.setState({ friends });
+      this.setState({ guessesCorrect });
+      this.setState({ message });
+    }
   };
   render() {
     return (
       <Wrapper>
         <Title>Game Cards</Title>
-        {this.state.friends.map(friend => (
-          <FriendCard
-            removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
-          />
-        ))}
+        <h3 className="message">{this.state.message}</h3>
+        <div className="row">
+          {this.state.friends.map(friend => (
+            <FriendCard
+              setClicked={this.setClicked}
+              id={friend.id}
+              key={friend.id}
+              image={friend.image}
+              name={friend.name}
+              className="col-sm-1"
+            />
+          ))}
+        </div>
       </Wrapper>
     );
   }
+
+  // ********old code
+  // removeFriend = id => {
+  //   const friends = this.state.friends.filter(friend => friend.id !== id);
+  //   this.setState({ friends });
+  // };
+  // render() {
+  //   return (
+  //     <Wrapper>
+  //       <Title>Game Cards</Title>
+  //       {this.state.friends.map(friend => (
+  //         <FriendCard
+  //           removeFriend={this.removeFriend}
+  //           id={friend.id}
+  //           key={friend.id}
+  //           name={friend.name}
+  //           image={friend.image}
+  //           occupation={friend.occupation}
+  //           location={friend.location}
+  //         />
+  //       ))}
+  //     </Wrapper>
+  //   );
+  // }
 }
 
 export default App;
--- src/components/GameCard/GameCard.js
+++ src/components/GameCard/GameCard.js
@@ -2,27 +2,11 @@ import React from "react";
 import "./GameCard.css";
 
 const GameCard = props => (
-  <div className="card">
+  <div className="card" onClick={() => props.setClicked(props.id)}>
     <div className="img-container">
-      <img alt={props.name} src={props.image} />
+      <img alt={props.name} src={props.image} className="img" />
     </div>
-    <div className="content">
-      <ul>
-        <li>
-          <strong>Name:</strong> {props.name}
-        </li>
-        {/* <li>
-          <strong>Image:</strong> {props.image}
-        </li> */}
-        {/* <li>
-          <strong>Address:</strong> {props.location}
-        </li> */}
-      </ul>
-    </div>
-    <span onClick={() => props.removeFriend(props.id)} className="remove">
-      x
-    </span>
-    {/* <span className="remove">𝘅</span> */}
+    <div className="content">{props.name}</div>
   </div>
 );
 
